3601808ad0996e8a44ed39ef36e9ce81b432c13b
Bonyaah <abonyaah@st.ug.edu.gh>
2025-08-05 13:39:53 +0100

    Add files via upload

---
+++ TicketPriceCalculator/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter your age: ");
+        int age = int.Parse(Console.ReadLine());
+
+        if (age >= 65 || age <= 12)
+            Console.WriteLine("Ticket price: GHC7");
+        else
+            Console.WriteLine("Ticket price: GHC10");
+    }
+}
